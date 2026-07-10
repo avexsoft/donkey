83d516512126f5ee2aa11552cd572dc4fe0e5555
mxts <53546313+mxts@users.noreply.github.com>
2026-07-10 09:54:48 +0800

    overwrite only remarks, not default value

--- src/Donkey.php
+++ src/Donkey.php
@@ -7,22 +7,28 @@ use Avexsoft\Donkey\Models\Override;
 class Donkey
 {
     /**
-     * Let the packages specific the kind of keys they are expecting the user to configure
+     * Let the packages specify the kind of keys they are expecting the user to configure
      * Call this in the `boot()` of service providers
      *
      * @param  string  $key  Laravel config() key that is used/referenced in our code
-     * @param  string  $remarks  give the user some idea what this is for
-     * @param  string  $defaultValue  default value added
+     * @param  string  $remarks  give the user some idea what this is for, always overwritten
+     * @param  string  $defaultValue  provide a default value (if record does not already exists)
      */
     public function expect($key, $remarks = null, $defaultValue = null): static
     {
-        $data = [];
-        $data['value'] = $defaultValue;
-        if ($remarks) {
-            $data['remarks'] = $remarks;
-        }
+        $existing = Override::where('key', $key)->first();
 
-        Override::firstOrCreate(['key' => $key], $data);
+        if ($existing) {
+            if ($remarks) {
+                $existing->update(['remarks' => $remarks]);
+            }
+        } else {
+            $data = ['key' => $key, 'value' => $defaultValue];
+            if ($remarks) {
+                $data['remarks'] = $remarks;
+            }
+            Override::create($data);
+        }
 
         return $this;
     }
