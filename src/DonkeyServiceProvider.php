8606c5235a3b49b015eea2a479a7ba7b211b3fb4
Lightz2002 <Lightz2002@users.noreply.github.com>
2026-07-25 03:49:38 +0000

    Fix styling

--- src/DonkeyServiceProvider.php
+++ src/DonkeyServiceProvider.php
@@ -35,7 +35,7 @@ class DonkeyServiceProvider extends ServiceProvider
         $this->mergeConfigFrom(__DIR__.'/../config/override.php', 'override');
 
         $this->app->singleton('donkey', function ($app) {
-            return new Donkey;
+            return new Donkey();
         });
 
         $this->app->booting(function () {
